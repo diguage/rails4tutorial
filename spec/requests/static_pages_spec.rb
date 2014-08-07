2e10e9f233879ff00eb10f492222f440fd9d0e82
D瓜哥 <leejun119@gmail.com>
2014-08-07 23:08:50 +0800

    测试驱动开发

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,4 +16,11 @@ describe "StaticPages" do
     end
   end
   
+  describe "About page" do
+    it "should have the content 'About Us" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+  
 end
