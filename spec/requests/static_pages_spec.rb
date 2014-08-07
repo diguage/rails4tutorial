3b71b2e003efec2f5cadba2ffd2a5335c8273b26
D瓜哥 <leejun119@gmail.com>
2014-08-08 07:51:25 +0800

    完成第一个练习：添加contact页面

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "StaticPages" do
     end
   end
   
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
+  
 end
